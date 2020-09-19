54112f3d295999aaa71adb8971857736667bb530
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2020-09-19 13:41:48 -0700

    Commit extra files

--- src/languageConstructs/TokenNames.kt
+++ src/languageConstructs/TokenNames.kt
@@ -3,7 +3,7 @@ package languageConstructs
 class Keywords {
     companion object {
         val keywords = listOf("var", "if", "else", "while", "break", "true", "false", "nil",
-            "as", "main", "func", "return", "void", "raw", "class", "struct", "protocol",
+            "as", "main", "func", "return", "void", "raw", "class", "struct", "enum", "protocol",
             "public", "private", "protected", "static", "import")
     }
 }
